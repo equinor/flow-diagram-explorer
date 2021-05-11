6ec47cfd63f84ed91c82ae1acbfeb14f0542eaad
Ruben <ruben.thoms@ceetronsolutions.com>
2021-05-11 16:05:04 +0200

    Setup of React build environment
    - Created React application structure with TypeScript
    - Setup GitHub workflows (build, format, test, deploy and verify changelog changes)
    - Installed testing framework (Jest + React-testing-library)
    - eslint + prettier
    - Setup and configured Webpack 5 (using Babel)
    - Created changelog file
    - Added scripts for publishing, testing, formatting, validating and transpiling

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [UNRELEASED] - YYYY-MM-DD
+
+### Added
+
+- [#3](https://github.com/equinor/flow-diagram-explorer/pull/3) - Setup of build environment.
---
+++ webpack.config.js
@@ -0,0 +1,121 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const packagejson = require("./package.json");
+const libraryName = packagejson.name.replace(/[-\/]/g, "_").replace(/@/g, "");
+
+module.exports = (env, argv) => {
+    const overrides = module.exports || {};
+
+    // Mode
+    let mode;
+    if (argv && argv.mode) {
+        mode = argv.mode;
+    }
+    else if (overrides.mode) {
+        mode = overrides.mode;
+    }
+    else {
+        mode = "production";
+    }
+
+    // Entry
+    const entry = {
+        main: argv && argv.entry ? argv.entry : "./dist/index.js",
+    };
+
+    // Output
+    const filenameJs = `${libraryName}.${mode === "development" ? "dev" : "min"}.js`;
+    const filenameCss = `${libraryName}.css`;
+
+    // Devtool
+    const devtool =
+        argv.devtool || (mode === "development" ? "eval-source-map" : false);
+
+    // NOTE: Keep order of the following configuration output
+    // See: https://webpack.js.org/configuration/
+    return {
+        mode: mode,
+        entry,
+        output: {
+            path: path.resolve(__dirname, "build"),
+            filename: filenameJs,
+            library: {
+                type: "window",
+                name: libraryName,
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: "babel-loader"
+                },
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader", "ts-loader"],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader:
+                                mode === "production"
+                                    ? MiniCssExtractPlugin.loader
+                                    : "style-loader",
+                        },
+                        "css-loader",
+                    ],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    use: {
+                        loader: 'url-loader',
+                    },
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".jsx"],
+        },
+        devtool: devtool,
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: filenameCss,
+            }),
+            new HtmlWebpackPlugin({
+                filename: path.resolve(__dirname, "build", "index.html"),
+                template: path.resolve(__dirname, "public", "index.html"),
+                inject: true,
+                favicon: path.resolve(__dirname, "public", "favicon.ico"),
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: "public", 
+                        globOptions: {
+                            ignore: ["**.html"]
+                        }
+                    }
+                ]
+            })
+        ],
+        optimization: {
+            minimizer: [
+                () => {
+                    return () => {
+                        return {
+                            terserOptions: {}
+                        }
+                    }
+                },
+                new CssMinimizerPlugin({}),
+            ],
+        },
+    };
+};
