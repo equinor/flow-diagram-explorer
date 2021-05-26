a063d403e70184c97066a1d2bdcf9bad5a042b28
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2021-05-26 11:35:09 +0200

    Adjusted format and changed invalid `eslint` setting.

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -8,13 +8,13 @@
 import React from "react";
 
 function App(): JSX.Element {
-  return (
-    <React.StrictMode>
-      <div className="App">
-        <header className="App-header">Here we are going to display the flow diagram explorer.</header>
-      </div>
-    </React.StrictMode>
-  );
+    return (
+        <React.StrictMode>
+            <div className="App">
+                <header className="App-header">Here we are going to display the flow diagram explorer.</header>
+            </div>
+        </React.StrictMode>
+    );
 }
 
 export default App;
