832e6f55b135acfcbf5308d69bcf0e545666ada9
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2021-06-10 11:37:36 +0200

    Removed license notice in files

--- src/lib/render-library/index.ts
+++ src/lib/render-library/index.ts
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2021- Equinor ASA
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import { cableRenderer } from "./Cable";
 import { compressorRenderer } from "./Compressor";
 import { compressorSystemRenderer } from "./CompressorSystem";
