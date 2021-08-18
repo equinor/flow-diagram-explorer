0990293b91bddacc40bb27923220d6522698e524
rubenthoms <69145689+rubenthoms@users.noreply.github.com>
2021-08-18 14:20:44 +0200

    Zoom on double click (#10)
    * Implementation of zoom on double click on view

--- src/lib/hooks/useZoom.ts
+++ src/lib/hooks/useZoom.ts
@@ -1,5 +1,9 @@
 import React from "react";
 
+const quad = (x: number): number => {
+    return -0.2 * x * x + 3.2;
+};
+
 export const useZoom = ({
     ref,
     minScale = 0.5,
@@ -19,13 +23,28 @@ export const useZoom = ({
             setScale(Math.min(maxScale, Math.max(minScale, scale - Math.sign(e.deltaY) * delta)));
         };
 
+        const handleDblClickEvent = (e: MouseEvent) => {
+            e.preventDefault();
+            let i = 0;
+            let newScale = scale;
+            const timer = setInterval(() => {
+                newScale = newScale + (quad(-4 + i++) / 4) * delta;
+                setScale(Math.min(maxScale, Math.max(minScale, newScale)));
+                if (i > 8) {
+                    clearInterval(timer);
+                }
+            }, 50);
+        };
+
         if (ref.current) {
             ref.current.addEventListener("wheel", handleWheelEvent);
+            ref.current.addEventListener("dblclick", handleDblClickEvent);
         }
 
         return () => {
             if (ref.current) {
                 ref.current.removeEventListener("wheel", handleWheelEvent);
+                ref.current.removeEventListener("dblclick", handleDblClickEvent);
             }
         };
     }, [ref, scale]);
